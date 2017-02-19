1d3a49cd932d65bce33cae50f10e2f9028132469
Yoann Picquenot <yoann.picquenot@gmail.com>
2017-02-19 10:18:36 +0000

    Base commit

--- app.js
+++ app.js
@@ -5,19 +5,21 @@ var config = require('./config');
 
 server.use(express.static(__dirname));
 
-server.get('/', function(req, res) {
-	res.sendFile('./index.html');
+server.get('/', function (req, res) {
+    res.sendFile('./index.html');
 });
 
-server.put('/benchmark', function(req, res) {
-	console.log(req);
-	res.sendStatus(200);
+server.put('/benchmark', function (req, res) {
+    console.log(req);
+    res.sendStatus(200);
 });
+
+server.get('/benchmark', function (req, res) {});
 
 server.listen(process.env.PORT || config.server.port, (err) => {
-	if(err) {
-		console.err(err);
-	} else {
-		console.log("Listening on port " + config.server.port);
-	}
+    if (err) {
+        console.err(err);
+    } else {
+        console.log("Listening on port " + config.server.port);
+    }
 });
