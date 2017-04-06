0cf2bfd2ff39c9893804ca69f8d9989eae90c18a
cchardeau <corentin.chardeau@epitech.eu>
2017-04-06 06:37:14 +0200

    Add bootstrap to the project

--- application/src/app/gpus/gpus.ts
+++ application/src/app/gpus/gpus.ts
@@ -21,15 +21,18 @@ export class GpusComponent {
 
   constructor(public http: Http) {
     this.getGpus().subscribe(result => {
-      console.log(result);
+      console.log('RESULT ', result);
       this.gpus = result;
     });
   }
 
   getGpus(): Observable<Gpu[]> {
     return this.http
-      // .get('http://localhost:5000/api/v1/gpus') When API is fixed
-      .get('app/gpus/gpus.json')
-      .map(response => response.json());
+      .get('http://localhost:5000/api/v1/gpus')
+      // .get('app/gpus/gpus.json')
+      .map(response => {
+        console.log('RESPONSE ', response);
+        return response.json();
+      });
   }
 }
