14ba707d01e6d939716325b09da6d4952d5b3675
cchardeau <corentin.chardeau@epitech.eu>
2017-04-22 07:21:39 +0200

    Add link to gpu

--- application/src/app/gpus/gpus.ts
+++ application/src/app/gpus/gpus.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
+import {Router} from "@angular/router";
 
 export class Gpu {
   constructor(
@@ -22,7 +23,7 @@ export class GpusComponent {
   public gpus: Gpu[];
   public gpu: Gpu;
 
-  constructor(public http: Http) {
+  constructor(public http: Http, public router: Router) {
     this.getGpus().subscribe(data => {
       this.gpus = data;
       return data;
@@ -34,4 +35,9 @@ export class GpusComponent {
       .get('http://localhost:5000/api/v1/gpus')
       .map(response => response.json().data);
   }
+
+  public goToGpu(event, id) {
+    this.router.navigateByUrl(`/gpus/${id}`);
+  }
+
 }
