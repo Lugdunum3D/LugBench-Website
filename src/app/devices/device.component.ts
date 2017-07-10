9825fe2ff748d77b1d1c9dc02358f8ad1e2ac4e6
Yoann Picquenot <yoann.picquenot@gmail.com>
2017-07-10 18:38:49 +0200

    Fix fix and limits properties

--- src/app/devices/device.component.ts
+++ src/app/devices/device.component.ts
@@ -15,9 +15,13 @@ export class DeviceComponent {
     private device: Device;
     private tabIndex: Number;
     private featureKeys: Array<String>;
+    private sparsePropertyKeys: Array<String>;
+    private limitKeys: Array<String>;
 
     constructor(public http: Http, public router: Router, private activatedRoute: ActivatedRoute) {
         this.featureKeys = new Array<String>();
+        this.sparsePropertyKeys = new Array<String>();
+        this.limitKeys = new Array<String>();
         this.tabIndex = 1;
         this.getDevice();
     }
@@ -31,8 +35,20 @@ export class DeviceComponent {
             console.log(this.device);
 
             for (let key in this.device.vulkanInfo.features) {
-                if (this.device.vulkanInfo.features) {
+                if (this.device.vulkanInfo.features !== null) {
                     this.featureKeys.push(key);
+                }
+            }
+
+            for (let key in this.device.vulkanInfo.properties.sparseProperties) {
+                if (this.device.vulkanInfo.properties.sparseProperties[key] !== null) {
+                    this.sparsePropertyKeys.push(key);
+                }
+            }
+
+            for (let key in this.device.vulkanInfo.properties.limits) {
+                if (this.device.vulkanInfo.properties.limits[key]) {
+                    this.limitKeys.push(key);
                 }
             }
         });
