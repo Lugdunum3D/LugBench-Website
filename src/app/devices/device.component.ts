7d0ebe717c53e8e29e0f7313ea2ebf7a4d7ce172
Yoann Picquenot <yoann#searchbios.com>
2017-10-20 10:22:49 +0200

    Add new tabs & and fix memory tab

--- src/app/devices/device.component.ts
+++ src/app/devices/device.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -28,8 +28,10 @@ export class DeviceComponent {
         this.getDevice();
     }
 
-    NgOnInit() {
-        (<any>$('.tabs')).tabs();
+    ngOnInit() {}
+
+    ngOnDestroy() {
+        $('header, main,  footer').css('padding-left', '0px');
     }
 
     getDevice(): void {
